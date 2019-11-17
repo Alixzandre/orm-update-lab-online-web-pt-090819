754b70341ea8c9872114119d7c8c5767da97d355
Alexander Bispo <alexbispo173@gmail.com>
2019-11-17 03:01:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,14 +31,14 @@ attr_reader :id
   end
  
   def self.create(name:, album:)
-    song = Song.new(name, album)
-    song.save
-    song
+    student = Student.new(name, album)
+    student.save
+    student
   end
  
   def self.find_by_name(name)
-    sql = "SELECT * FROM songs WHERE name = ?"
+    sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Song.new(result[0], result[1], result[2])
+    Student.new(result[0], result[1], result[2])
   end
 end
